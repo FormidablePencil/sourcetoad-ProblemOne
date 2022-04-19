9f630e8c036031730f2f913c857990d09c73f593
Dennis <dennis.aleksandrov@bit-wizards.com>
2022-04-19 16:36:07 -0500

    finished problem 1

---
+++ index.js
@@ -0,0 +1,86 @@
+var arr = [
+  {
+    guest_type: "crew",
+    first_name: "Marco",
+    last_name: "Burns",
+    guest_booking: {
+      room_no: "A0073",
+      some_array: [7, 2, 4],
+    },
+  },
+  {
+    guest_type: "guest",
+    first_name: "John",
+    last_name: "Doe",
+    guest_booking: {
+      room_no: "C73",
+      some_array: [1, 3, 5, 2, 4, 3],
+    },
+  },
+  {
+    guest_type: "guest",
+    first_name: "Jane",
+    last_name: "Doe",
+    guest_booking: {
+      room_no: "C73",
+      some_array: [1, 3, 5, 2, 4, 3],
+    },
+  },
+  {
+    guest_type: "guest",
+    first_name: "Albert",
+    last_name: "Einstein",
+    guest_booking: {
+      room_no: "B15",
+      some_array: [2, 5, 6, 3],
+    },
+  },
+  {
+    guest_type: "crew",
+    first_name: "Jack",
+    last_name: "Daniels",
+    guest_booking: {
+      room_no: "B15",
+      some_array: [2, 5, 6, 3],
+    },
+  },
+  {
+    guest_type: "guest",
+    first_name: "Alan",
+    last_name: "Turing",
+    guest_booking: {
+      room_no: "B15",
+      some_array: [2, 5, 6, 3],
+    },
+  },
+];
+
+class Mutator {
+  a;
+
+  constructor(a) {
+    this.a = a;
+  }
+
+  problem1() {
+    return this.a.map((element) => {
+      element.room_no = element.guest_booking.room_no;
+      element.some_array = [...element.guest_booking.some_array];
+      delete element["guest_booking"];
+      return element;
+    });
+  }
+
+  problem2() {}
+
+  problem3() {}
+}
+
+function mutateArray(a) {
+  return new Mutator(a).problem1();
+}
+
+$(document).ready(function () {
+  $("#originalArray").html(JSON.stringify(arr, null, 2));
+  $("#resultsArray").html(JSON.stringify(mutateArray(arr), null, 2));
+});
